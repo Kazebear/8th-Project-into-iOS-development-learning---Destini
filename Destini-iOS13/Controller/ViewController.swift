f6136106786801b69ced51e979836a49f4fed55f
Mara <kazee.bear@mail.com>
2021-11-15 23:47:21 +0200

    comit & push

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -3,6 +3,7 @@
 //  Destini-iOS13
 //
 //  Created by Angela Yu on 08/08/2019.
+//  Modified by Mariangela Duca
 //  Copyright © 2019 The App Brewery. All rights reserved.
 //
 
@@ -10,10 +11,41 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
+    @IBOutlet weak var storyLabel: UILabel!
+    @IBOutlet weak var choice1Button: UIButton!
+    @IBOutlet weak var choice2Button: UIButton!
+    
+    var example = StoryBrain()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUI()
     }
+    
+    
+   
+    @IBAction func choiceMade(_ sender: UIButton) {
+        storyVariable = example.choiceMadeOutcome(sender.currentTitle!)
+        print(sender.currentTitle!)
+        updateUI()
+        
+    }
+    
+    func updateUI(){
+        storyLabel.text = example.story[storyVariable].title
+        choice1Button.setTitle(example.story[storyVariable].choice1, for: .normal)
+        choice2Button.setTitle(example.story[storyVariable].choice2, for: .normal)
+    }
+    
+    
+    
+        
+
+    
+  
 
 
 }
